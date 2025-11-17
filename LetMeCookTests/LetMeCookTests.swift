109ec5f774091ac9616fc6a29bac84cb89e962e6
avayu2004 <avayu@berkeley.edu>
2025-11-17 11:19:54 -0800

    Initial Commit

---
+++ LetMeCookTests/LetMeCookTests.swift
@@ -0,0 +1,17 @@
+//
+//  LetMeCookTests.swift
+//  LetMeCookTests
+//
+//  Created by Ava Yu on 11/17/25.
+//
+
+import Testing
+@testable import LetMeCook
+
+struct LetMeCookTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
